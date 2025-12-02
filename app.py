54e5f29b0c286e07a4e7ba03930005693db1dfdc
Chintha Krishna Balaji <krishnabalajiwork@gmail.com>
2025-12-03 03:10:33 +0530

    Update import statements for langchain modules

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ All internship requirements still satisfied (see inline comments).
 import streamlit as st
 from openai import OpenAI
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 import time
+
 
 # ---------- PAGE CONFIG ----------
 st.set_page_config(
